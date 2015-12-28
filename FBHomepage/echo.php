268146d9383550f32e5cb627357c00a66fdc13f7
Ondřej Borýsek <ondrej.borysek+github@gmail.com>
2015-12-28 12:35:56 +0100

    FB Homepage - more fluent version
    Editing way proof of password is presented and how user is redirected to
    FB itself.

---
+++ FBHomepage/echo.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Falešný FB - vaše heslo</title>
+</head>
+<body>
+<?php
+	echo "Váš email: ".$_GET["email"];
+	echo "<br>";
+	echo "Vaše heslo: ".$_GET["pass"];
+?>
+</body>
+</html>
